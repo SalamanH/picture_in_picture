28a53752650dffcbf89660c244df32e23af7b8b8
salaman Hamidkohzad <salaman.hamidkohzad@gmail.com>
2020-10-08 15:13:14 -0400

    first commit

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-console.log('testing');
+const videoElement = document.getElementById('video');
+const button = document.getElementById('button');
+
+// Prompt user to select media stream, pass to video element, then play
+
+async function selectMediaStream()
+{
+    try {
+        const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+        videoElement.srcObject = mediaStream;
+        videoElement.onloadedmetadata = () => 
+        {
+            videoElement.play();
+        }
+    } catch (error) {
+        // catch Error here
+        console.log('whoops, error here: ', error);
+    }
+}
+
+button.addEventListener('click', async () => {
+    // disable button
+    button.disabled = true;
+    // start picture in picture 
+    await videoElement.requestPictureInPicture();
+    // reset button
+    button.disabled = false;
+});
+// on load
+selectMediaStream();
